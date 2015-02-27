27bfcc4a709859435fcbb9919a629e21f2b82899
Navodar22 <martisradovan@gmail.com>
2015-02-28 00:01:15 +0100

    Disabled Library look

--- src/main/java/diplomovka/Diplomovka.java
+++ src/main/java/diplomovka/Diplomovka.java
@@ -5,16 +5,11 @@
 package diplomovka;
 
 //import com.jtattoo.plaf.acryl.AcrylLookAndFeel;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
 
 import design.MainFrame;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.jtattoo.plaf.acryl.AcrylLookAndFeel;
 
 /**
  *
@@ -30,15 +25,6 @@ public class Diplomovka {
     
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
-
-
-        // TODO code application logic here
-        String laf = UIManager.getCrossPlatformLookAndFeelClassName();
-		try {
-			UIManager.setLookAndFeel(new AcrylLookAndFeel());
-		} catch (UnsupportedLookAndFeelException ex) {
-			Logger.getLogger(Diplomovka.class.getName()).log(Level.SEVERE, null, ex);
-		}
 
 		MainFrame mainFrame = MainFrame.getInstance();
 		mainFrame.setMainFrame();
